85cbaf6dc6ea9703c90e5d2496159c2134c5daaa
matheusp <matheus.pedroso.silva21@gmail.com>
2025-06-12 16:54:29 -0300

    Upload das páginas de atração e oficinas

---
+++ atracoes.php
@@ -0,0 +1,44 @@
+<!-- INICIO - PirulitosDeAcucarEMel -->
+
+<section id="oficinas">
+    <h2 class="text-center">ATRAÇÕES</h2>
+    <p class="text-center">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur perspiciatis quam doloremque! Labore, reprehenderit fugit? Dolores nobis tempora explicabo vero adipisci ipsa tempore, ullam temporibus cupiditate labore quasi libero quo.
+    </p>
+
+    <div class="row border border-primary">
+
+    <div class="col-md-6 col-sm-12 d-flex justify-content-center border border-primary">
+        <figure class="figure">
+        <img src="https://placehold.co/500x300" class="figure-img img-fluid rounded" alt="placehold">
+        </figure>
+    </div>
+
+    <div class="col-md-6 col-sm-12 border border-danger">
+
+        <div class="row border border-primary">
+        <div class="col-6 p-1 d-flex justify-content-center border border-primary">
+            <figure class="figure">
+            <img src="https://placehold.co/200x150" class="figure-img img-fluid rounded" alt="placehold">
+            </figure>
+        </div>
+        <div class="col-6 p-1 d-flex justify-content-center border border-primary">
+            <figure class="figure">
+            <img src="https://placehold.co/200x150" class="figure-img img-fluid rounded" alt="placehold">
+            </figure>
+        </div>
+        </div>
+
+        <div class="row border border-primary">
+        <h3 class="text-center pt-2">Titulo</h3>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus omnis cupiditate quo placeat mollitia voluptatibus earum, tempora accusantium magnam officia error ex, debitis eveniet assumenda aspernatur at corporis. Cumque, error?
+        </p>
+        </div>
+
+    </div>
+    </div>
+
+</section>
+
+<!-- FIM - PirulitosDeAcucarEMel -->
